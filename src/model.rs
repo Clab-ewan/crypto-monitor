50574ca99ce057e2ad0fc9b153bb957f36a7b369
Clabaut Ewan <clabewan@gmail.com>
2026-04-13 13:54:36 +0200

    Implement PortfolioRow struct and build method for portfolio creation from assets and API data

--- src/model.rs
+++ src/model.rs
@@ -1,3 +1,5 @@
+use crate::api::CoinPrices;
+use crate::config::Config;
 use serde::Deserialize;
 
 #[derive(Deserialize, Debug)]
@@ -8,8 +10,6 @@ pub struct Asset {
     pub amount: f64,
 }
 
-
-
 #[derive(Debug)]
 pub struct PortfolioRow {
     id: String,
@@ -17,4 +17,21 @@ pub struct PortfolioRow {
     price: f64,
     curr_24h_change: f64,
     value: f64,
+}
+
+impl PortfolioRow {
+    pub fn build(assets: &Vec<Asset>, api_res: CoinPrices, config: Config) -> Vec<PortfolioRow> {
+        let mut portfolio: Vec<PortfolioRow> = Vec::new();
+        for a in assets {
+            let row: PortfolioRow = PortfolioRow {
+                id: a.id.clone(),
+                amount: a.amount,
+                price: api_res[&a.id][&config.currency],
+                curr_24h_change: api_res[&a.id][&format!("{}_24h_change", config.currency)],
+                value: a.amount * api_res[&a.id][&config.currency],
+            };
+            portfolio.push(row);
+        }
+        portfolio
+    }
 }
